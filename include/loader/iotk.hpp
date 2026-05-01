f61c9b258a2d0772b5c6ce0edc7e2fdf115bb8c8
Samuel Bourasseau <bourasses@pm.me>
2026-05-01 11:14:16 +0900

    add iotk::MouseDown()

--- include/loader/iotk.hpp
+++ include/loader/iotk.hpp
@@ -70,6 +70,11 @@ static inline bool MouseHold(fMouseButton button_, input_t const& input_, input_
     return ((input_.button_down & button_) != 0) && ((past_input_.button_down & button_) != 0);
 }
 
+static inline bool MouseDown(fMouseButton button_, input_t const& input_)
+{
+    return ((input_.button_down & button_) != 0);
+}
+
 static inline numtk::vec2i MouseDelta(input_t const& input_, input_t const& past_input_)
 {
     return *(numtk::vec2i*)input_.cursor - *(numtk::vec2i*)past_input_.cursor;
